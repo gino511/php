672c6534f2c54f017aa821d2427b91b5ff4e9fbc
gino511 <122242248+gino511@users.noreply.github.com>
2025-04-10 21:59:14 +0800

    Add files via upload

---
+++ php/1.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>我的第一個php程式</title>
+    </head>      
+    <body>
+        <h1>我的第一個php程式</h1>
+        <h2>
+        <?php 
+            echo "Hello world!"; 
+        ?> 
+        </h2>        
+    </body>
+</html>
